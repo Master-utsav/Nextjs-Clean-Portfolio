713505ac9d5e92e5e9c85d4e51df482318c57195
master-utsav <utsavjaiswal890@gmail.com>
2024-11-10 01:36:52 +0530

    techCard_scrollTrigger

--- src/sections/LoadingSection.tsx
+++ src/sections/LoadingSection.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const LoadingSection = () => {
   return (
     <div
-      className="flex items-center justify-center min-h-screen bg-[#121212] ">
+      className="flex items-center justify-center min-h-screen bg-[#121212] px-2">
         <svg
         width="577"
         height="114"
