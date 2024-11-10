2b1115be1e0895c2098feed1da6f13c9317108f1
master-utsav <utsavjaiswal890@gmail.com>
2024-11-11 01:18:14 +0530

    adding_about_section_cards

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -14,7 +14,7 @@ const AboutSection: React.FC = () => {
     <section
       ref={titleRef}
       id="about"
-      className="min-h-screen relative max-w-6xl text-center flex justify-center flex-col pt-20 space-y-4 items-center font-[family-name:var(--font-maven-pro)]"
+      className="min-h-screen relative max-w-6xl text-center flex justify-center flex-col pt-20 space-y-4 items-center font-[family-name:var(--font-maven-pro)] "
     >
       <div
         ref={titleRef}
@@ -115,7 +115,7 @@ const AboutSection: React.FC = () => {
               </Button>
             </div>
           ))}
-          <div className="w-full max-w-xs relative">
+          <div className="w-full max-w-lg relative">
             <div className="absolute inset-0 flex items-center justify-center pulse-effect-collapse z-0" />
             <Button
               variant="gooeyRight"
