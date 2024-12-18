c56773183d3ee3236f7532388d3ed0d8c6bb09dc
master-utsav <utsavjaiswal890@gmail.com>
2024-12-18 13:30:49 +0530

    fixing_posts_button

--- src/components/ui/PostsButton.tsx
+++ src/components/ui/PostsButton.tsx
@@ -27,7 +27,7 @@ const PostsButton = ({
       {type === "fixed" ? (
         <Button
         variant={"expandIcon"}
-        className={`w-full electric-lightning-effect rounded-md dark:text-white text-black dark:bg-black-200 bg-white-600/30 text-base transition-all delay-100 duration-500 ease-in-out   dark:hover:bg-black-100 hover:bg-white-600/50 sm:flex hidden font-[family-name:var(--font-assistant)]  ${
+        className={`w-full electric-lightning-effect rounded-md dark:text-white text-black dark:bg-black-200 bg-white-600/30 text-base transition-all delay-100 duration-500 ease-in-out   dark:hover:bg-black-100 hover:bg-white-600/50 flex font-[family-name:var(--font-assistant)]  ${
           isActive
             ? "border-[2px] dark:border-blue-500/60 border-blue-800/60"
             : "border-[1px] dark:border-blue-500/30 border-blue-800/30"
